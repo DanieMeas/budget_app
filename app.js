35b309e4f1ccafbdbfc3e27d87459a4db62461a3
daniemeas <danie.meas@gmail.com>
2019-04-09 21:03:09 -0500

    delete budget controller

--- app.js
+++ app.js
@@ -69,6 +69,28 @@ return {
 
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+
+      //id = 6
+      //data.allItems[type][id];
+      //ids = [1 2 4 6 8]
+      //index = 3
+
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+
+      });
+      
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+          data.allItems[type].splice(index, 1);
+
+      }
+
+    },
+
     calculateBudget: function(){
 
       //calculate total income and expenses
@@ -258,7 +280,14 @@ var controller = (function(budgetCtrl, UICtrl) {
       //inc-1
       splidID = itemID.split('-');
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
+
+      //1. Delete the item from data structure
+      budgetCtrl.deleteItem(type, ID);
+
+      //2. Delete the item from the UI
+
+      //3. Update and show the new budget
 
 
     }
